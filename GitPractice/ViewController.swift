f5b9ca0da4a6f4b031dd8f1f5eacbfcd1de22176
Gary Lin <ban103123@gmail.com>
2023-07-08 17:46:58 +0800

    我的第一版

---
+++ GitPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitPractice
+//
+//  Created by imac-3570 on 2023/7/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("123")
+    }
+
+
+}
+
